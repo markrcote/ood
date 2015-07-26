e34f4b26efafb54088af307efa21b76b9cafa1be
Mark Côté <mcote@alumni.uwaterloo.ca>
2015-07-26 03:07:00 -0400

    Add README.md, core droplet controller, and some test stuff.

---
+++ README.md
@@ -0,0 +1,109 @@
+# OoD: Ore on Demand
+
+OoD is a system that creates a Minecraft server on demand and archives
+and destroys it when is no longer in use.
+
+OoD will also be a simple web app that allows authorized users to start and
+stop the game.
+
+The primary audience for OoD is a small group of casual Minecrafters, for
+which a dedicated server would sit idle most of the time.  OoD minimizes
+this idle time, greatly reducing the cost of running a server.
+
+For example, a 1 GB droplet normally costs $10/month.  If the server only
+gets used 10 hours a week, OoD would reduce this cost to less than $0.70/month,
+including the idle period before OoD decides to shut down the server.  This
+also makes it much more affordable to go with a heavier server, if desired. A
+4 GB/2 CPU system would cost less than $3/month with the same usage.
+
+When a user wants to play, they log into the web app.  If the server
+is already running, the IP is displayed.  If not, they can start up
+the server from the previous archive.  After a certain amount of time
+without players, the host is archived and destroyed.
+
+## Status
+
+OoD is in the early stages.
+
+* Host controller: core is functional
+* Web app: not started
+* Droplet creation: not started
+
+### 2015/07/26
+
+I have the core host controller working, although it needs something to drive
+it (`update_status()` every X minutes) and a way to accept commands (probably
+a simple named queue, for now at least).
+
+The system currently presumes the following:
+
+* There is at least one snapshot of a Minecraft droplet named
+  `ood-<timestamp>`, where `<timestamp>` is a standard UNIX timestamp in
+  seconds.
+
+* This image needs to be running Minecraft via supervisord under the
+  name `minecraft`.  It needs to be running on the port specified in
+  `ood.droplet.MINECRAFT_PORT`, and it needs RCON running on
+  `ood.droplet.MINECRAFT_RCON_PORT`.  An RCON password should also be
+  set to a random string.  Edit server.properties:
+
+        server-port=<MINECRAFT_PORT>
+        enable-rcon=true
+        rcon.port=<MINECRAFT_RCON_PORT>
+        rcon.password=<RCON password>
+
+* The following files need to exist in `~/.ood`:
+  * `droplet_key`: Contains a DigitalOcean [Personal Access Token][].
+  * `rcon_pw`: Contains the RCON password set above.
+  * `ssh_key` and `ssh_key.pub`: SSH keys for root access to the
+  snapshotted image.
+
+* You need [MCRcon][] installed in your virtualenv.  This project
+ doesn't have a package, nor does it have a license, so I can't
+ include it here.  Hopefully I won't need to write my own.
+
+Eventually I'll build an ansible playbook to create an image from scratch.
+
+If you have all that set up, you can use OoD like so:
+
+    >>> import ood.test
+
+    >>> dc = ood.test.new()
+    2015-07-26 02:25:42:INFO:root:Droplet is not running.
+
+    >>> dc.start(); dc.wait_for_state('running', timeout=300, sleep_time=5)
+    2015-07-26 02:26:54:INFO:root:State changed from archived to restoring.
+    2015-07-26 02:27:05:INFO:root:Still in state restoring...
+    2015-07-26 02:27:16:INFO:root:Still in state restoring...
+    2015-07-26 02:27:27:INFO:root:Still in state restoring...
+    2015-07-26 02:27:38:INFO:root:Still in state restoring...
+    2015-07-26 02:27:49:INFO:root:State changed from restoring to starting.
+    2015-07-26 02:28:00:INFO:root:Still in state starting...
+    2015-07-26 02:28:05:INFO:root:State changed from starting to running.
+    2015-07-26 02:28:05:INFO:root:Minecraft available on <IP>:<port>.
+
+    >>> dc.wait_for_state('archived', timeout=3000, sleep_time=30, update_time=30)
+    2015-07-26 02:30:01:INFO:root:Still in state running...
+    2015-07-26 02:30:31:INFO:root:Still in state running...
+    2015-07-26 02:31:02:INFO:root:Still in state running...
+    2015-07-26 02:31:32:INFO:root:Still in state running...
+    2015-07-26 02:32:03:INFO:root:Still in state running...
+    2015-07-26 02:32:34:INFO:root:Still in state running...
+    2015-07-26 02:33:05:INFO:root:Still in state running...
+    2015-07-26 02:33:35:INFO:root:No players for the last 305 seconds.
+    2015-07-26 02:33:35:INFO:root:Shutting down host.
+    2015-07-26 02:33:36:INFO:root:State changed from running to shutting_down.
+    2015-07-26 02:34:06:INFO:root:State changed from shutting_down to snapshotting.
+    2015-07-26 02:34:37:INFO:root:Still in state snapshotting...
+    2015-07-26 02:36:10:INFO:root:Snapshot completed: droplet 6295938.
+    2015-07-26 02:36:10:INFO:root:State changed from snapshotting to destroying.
+    2015-07-26 02:36:41:INFO:root:State changed from destroying to archived.
+
+The `DropletController` class could probably be split into at least two
+classes, one for the state machine and one for the droplet/server/RCON
+interfacing.  That would also allow other cloud providers to be added.
+
+The system as a whole needs to be more configurable.
+
+[Personal Access Token]: https://cloud.digitalocean.com/settings/applications
+[MCRcon]: https://github.com/barneygale/MCRcon
---
+++ ood/droplet.py
@@ -0,0 +1,335 @@
+import errno
+import logging
+import os
+import re
+import socket
+import time
+from operator import attrgetter
+
+import digitalocean
+from mcrcon import MCRcon
+from paramiko.client import AutoAddPolicy, SSHClient
+
+
+DROPLET_NAME = 'ood'
+# TODO: Make all this configurable.
+REGION = 'nyc3'
+DATA_DIR = os.path.join(os.getenv('HOME'), '.ood')
+MINECRAFT_PORT = 25898
+MINECRAFT_RCON_PORT = 25899
+
+STATE_ARCHIVED = 'archived'
+STATE_RESTORING = 'restoring'
+STATE_BOOTING = 'booting'
+STATE_STARTING = 'starting'
+STATE_RUNNING = 'running'
+STATE_STOPPING = 'stopping'
+STATE_SHUTTING_DOWN = 'shutting_down'
+STATE_SNAPSHOTTING = 'snapshotting'
+STATE_DESTROYING = 'destroying'
+STATE_UNKNOWN = 'unknown'
+
+NUM_PLAYERS_RE = re.compile('There are (\d+)/\d+ players')
+MAX_SECONDS_NO_PLAYERS = 15*60
+
+
+class DropletController(object):
+    """Controls a DigitalOcean droplet running Minecraft.
+
+    Requires an existing snapshot of the form 'ood-<timestamp>'.
+
+    TODO: Split this into at least two classes, one for interfacing with
+    the DO API/server/game, and one for the state machine.
+    TODO: Timeouts aren't handled very well in a few places; they have a
+    tendency to drift.  Not a huge deal, but ugly.
+    """
+
+    def __init__(self, api_key_path, ssh_key_path, rcon_pw_path):
+        self.api_key_path = os.path.expanduser(api_key_path)
+        self.ssh_key_path = os.path.expanduser(ssh_key_path)
+        self.rcon_pw_path = os.path.expanduser(rcon_pw_path)
+        self.snapshot_action = None
+
+        self.last_time_seen_player = None
+
+        if not os.path.exists(DATA_DIR):
+            os.mkdir(DATA_DIR)
+
+        self._set_droplet()
+
+        self.state = STATE_ARCHIVED
+        if self.droplet:
+            if self.droplet.status == 'off':
+                logging.error('Droplet found but off; not sure what the '
+                              'current state is.')
+                self._set_state(STATE_UNKNOWN)
+            self.update_state()
+        else:
+            logging.info('Droplet is not running.')
+
+    def start(self):
+        """Starts up a droplet from the most recent snapshot."""
+        if self.state != STATE_ARCHIVED:
+            logging.error('Cannot start Minecraft in state %s.' % self.state)
+            return
+
+        snapshot = self._find_snapshot()
+        if snapshot is None:
+            logging.error('No ood snapshot found.')
+            return
+
+        ssh_key = self._find_ssh_key()
+        if ssh_key is None:
+            logging.error('No ood ssk key found.')
+            return
+
+        self.droplet = digitalocean.Droplet(token=self.api_key,
+                                            name='ood',
+                                            region=REGION,
+                                            image=snapshot.id,
+                                            size_slug='1gb',
+                                            ssh_keys=[ssh_key])
+        self.droplet.create()
+
+    def stop(self):
+        """Starts the stop-snapshot-destroy process."""
+        if self.state != STATE_RUNNING:
+            logging.error('Cannot stop Minecraft in state %s.' % self.state)
+            return
+
+        logging.info('Stopping Minecraft.')
+        self._exec_ssh_cmd('supervisorctl stop minecraft')
+        self._set_state(STATE_STOPPING)
+
+    def update_state(self):
+        # TODO: States should have their own timeouts.
+        self._set_droplet()
+
+        if self.state == STATE_UNKNOWN:
+            return
+
+        # Some conditions cause direct movement to a particular state.
+
+        if self.droplet is None or self.droplet.status == 'archive':
+            self._set_state(STATE_ARCHIVED)
+            return
+
+        if self.droplet.status == 'new':
+            self._set_state(STATE_RESTORING)
+
+        # Most state transitions depend on the current state and some
+        # other condition(s).
+
+        if self.droplet.status == 'off':
+            # State should either be SHUTTING_DOWN or SNAPSHOTTING.
+            if self.state != STATE_SNAPSHOTTING:
+                shutdown_error = False
+
+                if self.state != STATE_SHUTTING_DOWN:
+                    logging.warning('Droplet status changed to shutting down '
+                                    'while in state %s.' % self.state)
+                    shutdown_error = True
+
+                self._snapshot(shutdown_error)
+                self._set_state(STATE_SNAPSHOTTING)
+
+        if self.state == STATE_ARCHIVED or self.state == STATE_RESTORING:
+            if self.droplet.status == 'active':
+                self._set_state(STATE_STARTING)
+
+        if self.state == STATE_STARTING:
+            if self._minecraft_port_open():
+                self._set_state(STATE_RUNNING)
+                # We haven't seen a player yet, but we need to given them
+                # time to join.
+                self.last_time_seen_player = time.time()
+                logging.info('Minecraft available on %s:%d.' %
+                             (self.droplet_ip, MINECRAFT_PORT))
+
+        if self.state == STATE_RUNNING:
+            self._check_players()
+            if (time.time() > (self.last_time_seen_player +
+                               MAX_SECONDS_NO_PLAYERS)):
+                logging.info('No players for the last %d seconds.' %
+                             (time.time() - self.last_time_seen_player))
+                self._shutdown()
+            # TODO: maybe move to STOPPING after a certain number of socket
+            # errors.
+
+        if self.state == STATE_SNAPSHOTTING:
+            if self.snapshot_action is None:
+                logging.error('No snapshot action while in SNAPSHOTTING '
+                              'state!')
+                self._set_state(STATE_UNKNOWN)
+                # TODO: we could try to find an in-progress snapshot action
+                # associated with our droplet, if we have one.
+                return
+
+            self.snapshot_action.load()
+            if self.snapshot_action.status == 'completed':
+                logging.info('Snapshot completed: %s %s.' %
+                             (self.snapshot_action.resource_type,
+                              self.snapshot_action.resource_id))
+                self.droplet.destroy()
+                self._set_state(STATE_DESTROYING)
+            elif self.snapshot_action.status == 'errored':
+                logging.error('Error taking snapshot!')
+                self._set_state(STATE_UNKNOWN)
+
+        # DESTROYING should progress to ARCHIVED through a general state
+        # change above.
+
+    def wait_for_state(self, new_state, timeout=60, sleep_time=2,
+                       update_time=10):
+        start_time = time.time()
+        last_update_state = self.state
+        last_update_time = start_time
+        self.update_state()
+
+        while self.state != new_state and time.time() < start_time + timeout:
+            if last_update_state != self.state:
+                last_update_state = self.state
+                last_update_time = time.time()
+            elif time.time() >= last_update_time + update_time:
+                logging.info('Still in state %s...' % self.state)
+                last_update_time = time.time()
+            time.sleep(sleep_time)
+            self.update_state()
+
+        if self.state == new_state:
+            return True
+
+        logging.warning('Droplet did not change to state %s within %d '
+                        'seconds.  State is currently %s.' %
+                        (new_state, timeout, self.state))
+        return False
+
+    @property
+    def api_key(self):
+        return file(self.api_key_path).read().strip()
+
+    @property
+    def droplet_ip(self):
+        if self.droplet is None:
+            return None
+
+        for network in self.droplet.networks['v4']:
+            if network['type'] == 'public':
+                return network['ip_address']
+
+        return None
+
+    @property
+    def manager(self):
+        return digitalocean.Manager(token=self.api_key)
+
+    def _find_ssh_key(self):
+        for key in self.manager.get_all_sshkeys():
+            if key.name == 'ood':
+                return key
+        return None
+
+    def _find_snapshot(self):
+        snapshots = sorted([img for img in self.manager.get_my_images()
+                            if img.name.startswith('%s-' % DROPLET_NAME)],
+                           key=attrgetter('name'), reverse=True)
+
+        if not snapshots:
+            return None
+
+        return snapshots[0]
+
+    def _set_droplet(self):
+        for droplet in self.manager.get_all_droplets():
+            if droplet.name == DROPLET_NAME:
+                self.droplet = droplet
+                break
+        else:
+            self.droplet = None
+
+    def _set_state(self, new_state):
+        if self.state == new_state:
+            return
+
+        logging.info('State changed from %s to %s.' % (self.state, new_state))
+        self.state = new_state
+
+    def _minecraft_port_open(self):
+        if self.droplet_ip is None:
+            return False
+
+        try:
+            s = socket.create_connection((self.droplet_ip, MINECRAFT_PORT),
+                                         timeout=5)
+        except socket.error as e:
+            if e.errno != errno.ECONNREFUSED:
+                logging.warning('Unexpected socket error when checking '
+                                'Minecraft port: %s' % e)
+            return False
+        except socket.timeout:
+            return False
+
+        s.close()
+        return True
+
+    def _snapshot(self, shutdown_error):
+        if self.droplet is None:
+            logging.error('Cannot start snapshot: no droplet.')
+            return
+
+        if self.snapshot_action:
+            logging.error('Cannot start snapshot: snapshot in progress.')
+            return
+
+        snapshot_name = '%s-%d' % (DROPLET_NAME, time.time())
+        if shutdown_error:
+            snapshot_name += '-error'
+
+        self.snapshot_action = self.droplet.take_snapshot(snapshot_name,
+                                                          return_dict=False)
+
+    def _exec_ssh_cmd(self, cmdline):
+        client = SSHClient()
+        client.set_missing_host_key_policy(AutoAddPolicy())
+        client.connect(self.droplet_ip, username='root',
+                       key_filename=self.ssh_key_path)
+        stdin, stdout, stderr = client.exec_command(cmdline)
+        for line in stdout:
+            logging.info(line)
+        for line in stderr:
+            logging.info(line)
+
+    def _shutdown(self):
+        logging.info('Shutting down host.')
+        self.droplet.shutdown()
+        self._set_state(STATE_SHUTTING_DOWN)
+        # TODO: Keep the action object and use it to verify state (but
+        # double check that the state is 'off' after).
+        # TODO: Call self.droplet.poweroff() if shutdown() fails or takes
+        # too long.
+
+    def _num_players(self):
+        rcon = MCRcon()
+        try:
+            rcon.connect(self.droplet_ip, MINECRAFT_RCON_PORT)
+            rcon.login(file(self.rcon_pw_path).read().strip())
+            out = rcon.command('/list')
+        except socket.error as e:
+            # TODO: We should log errors if we get connection-refused
+            # errors for too long.
+            if e.errno != errno.ECONNREFUSED:
+                logging.warning('Unexpected socket error when accessing '
+                                'Minecraft RCON port: %s' % e)
+            return 0
+
+        m = NUM_PLAYERS_RE.match(out)
+
+        if not m:
+            logging.error('Invalid output from /list: %s' % out)
+            return
+
+        return int(m.group(1))
+
+    def _check_players(self):
+        if self._num_players() > 0:
+            self.last_time_seen_player = time.time()
