cad865d18577b156ed31544af54773b1c681c5c0
Mark Côté <mcote@alumni.uwaterloo.ca>
2015-08-20 22:42:08 -0400

    Add local controller.
    This standalone app manages a MineCraft server instance via a basic TCP
    server.  The protocol is line based, with "start" starting the server
    and "stop" stopping it.  Output from the app and stdout/stderr from MineCraft
    is recorded to the given path, in a file named `ood_local.log`.

---
+++ ood/local.py
@@ -0,0 +1,107 @@
+import asynchat
+import asyncore
+import datetime
+import os
+import socket
+import subprocess
+
+
+minecraft_proc = None
+
+
+class LocalControllerHandler(asynchat.async_chat):
+
+    def __init__(self, minecraft_path, minecraft_jar, sock):
+        asynchat.async_chat.__init__(self, sock=sock)
+        self.set_terminator('\n')
+        self.minecraft_path = minecraft_path
+        self.minecraft_jar = minecraft_jar
+        self.ibuffer = []
+        self.out = file(os.path.join(self.minecraft_path, 'ood_local.log'),
+                        'a')
+        self.log('OoD local controller starting.')
+
+    def log(self, msg):
+        self.out.write('[%s] OOD_LOCAL %s\n' % (datetime.datetime.now(), msg))
+
+    def start_mc(self):
+        global minecraft_proc
+
+        if minecraft_proc:
+            self.log('MineCraft server is already running!')
+            return
+
+        minecraft_proc = subprocess.Popen(
+            ['java', '-Xmx1024M', '-Xms1024M', '-jar', self.minecraft_jar,
+             'nogui'],
+            stdout=self.out,
+            stderr=subprocess.STDOUT,
+            cwd=self.minecraft_path)
+
+    def stop_mc(self):
+        global minecraft_proc
+
+        if not minecraft_proc:
+            self.log('Minecraft server is not running!')
+            return
+
+        minecraft_proc.terminate()
+        rc = minecraft_proc.wait()
+        self.log('MineCraft server exited with return code %d.' % rc)
+        minecraft_proc = None
+
+    def collect_incoming_data(self, data):
+        self.ibuffer.append(data)
+
+    def found_terminator(self):
+        cmd = ''.join(self.ibuffer).strip()
+        self.ibuffer = []
+        if cmd == 'start':
+            self.start_mc()
+        elif cmd == 'stop':
+            self.stop_mc()
+        else:
+            self.log('Unknown command "%s".' % cmd)
+
+
+class LocalControllerServer(asyncore.dispatcher):
+
+    def __init__(self, minecraft_path, minecraft_jar, port):
+        asyncore.dispatcher.__init__(self)
+        self.minecraft_path = minecraft_path
+        self.minecraft_jar = minecraft_jar
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(('127.0.0.1', port))
+        self.listen(0)
+
+    def handle_accept(self):
+        client_info = self.accept()
+        LocalControllerHandler(self.minecraft_path, self.minecraft_jar,
+                               client_info[0])
+
+
+if __name__ == '__main__':
+    import errno
+    import sys
+
+    if len(sys.argv) < 3:
+        print 'Usage: %s <path to minecraft> <port>' % sys.argv[0]
+        sys.exit(errno.EINVAL)
+
+    path = sys.argv[1]
+
+    try:
+        port = int(sys.argv[2])
+    except (TypeError, ValueError):
+        print '<port> must be an integer.'
+        sys.exit(errno.EINVAL)
+
+    if len(sys.argv) > 3:
+        jar_file = sys.argv[3]
+    else:
+        jar_file = 'minecraft_server.jar'
+
+    lcs = LocalControllerServer(path, jar_file, port)
+
+    asyncore.loop()
